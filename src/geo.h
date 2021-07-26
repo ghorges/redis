2012218e4215015ba853949a8e3bc10707068e31
ghorges <1298394633@qq.com>
2021-07-26 10:35:19 +0800

    添加注释

--- src/geo.h
+++ src/geo.h
@@ -3,6 +3,7 @@
 
 #include "server.h"
 
+// 在 geo.c 中使用的结构来表示点和数组地球上的点。网上说之前是 zset？
 /* Structures used inside geo.c in order to represent points and array of
  * points on the earth. */
 typedef struct geoPoint {
